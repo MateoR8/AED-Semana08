57d161079f7b146729868bce32c87aa1084bca82
Mateo Regalado Quispe <mateoquispe08@hotmail.com>
2023-10-21 08:28:51 -0500

    Problema 02 - resuelto

--- AED_Semana_08_Lab/Problema_8_2/src/semana_08/Arreglo.java
+++ AED_Semana_08_Lab/Problema_8_2/src/semana_08/Arreglo.java
@@ -1,37 +1,69 @@
 package semana_08;
 
 public class Arreglo {
-	
-	//  Atributos privados
+
+	// Atributos privados
 	private int[] n;
 	private int indice;
-	//  Constructor
+
+	// Constructor
 	public Arreglo() {
 		n = new int[10];
 		indice = 0;
 	}
-	//  Operaciones públicas básicas
+
+	// Operaciones públicas básicas
 	public int tamanio() {
 		return indice;
 	}
+
 	public int obtener(int i) {
 		return n[i];
 	}
-	public void adicionarDistintoYordenar(int numero) {
 
-	}
-	//  Operaciones privadas
+	// Operaciones privadas
 	private void ampliarArreglo() {
 		int[] aux = n;
 		n = new int[indice + 10];
-		for (int i=0; i<indice; i++)
+		for (int i = 0; i < indice; i++)
 			n[i] = aux[i];
 	}
+
 	public void adicionar(int numero) {
 		if (indice == n.length)
 			ampliarArreglo();
 		n[indice] = numero;
-		indice ++;
+		indice++;
 	}
-	
+
+	private void intercambiar(int num1, int num2) {
+		int aux = n[num1];
+		n[num1] = n[num2];
+		n[num2] = aux;
+	}
+
+	private int buscar(int numero) {
+		for (int i = 0; i < indice; i++)
+			if (n[i] == numero)
+				return i;
+		return -1;
+	}
+
+	public void menoraMayor() {
+		for (int i = 0; i < indice; i++) {
+			for (int j = 0; j < indice; j++) {
+				if (n[i] < n[j]) {
+					intercambiar(i, j);
+				}
+			}
+		}
+	}
+
+	public void adicionarDistintoYordenar(int numero) {
+		int num = buscar(numero);
+		if(num == -1) {
+			adicionar(numero);
+		}
+		menoraMayor();
+	}
 }
